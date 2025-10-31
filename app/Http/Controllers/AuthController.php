71d81466c228d19f0946b74b1e54e8a02d9e9ace
hasanali <62792109+hasanaliNejati@users.noreply.github.com>
2025-10-31 18:29:28 +0330

    Fix : login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
              return redirect()->route('home');
          }
 
-        //return view('auth.login');
+        return view('login');
     }
     public function logout(Request $request)
     {
