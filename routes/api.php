f882f83c8c9d6f01d85a2cd0688bfa70aca30966
mohammad14029 <mohammadsoleimani313m@gmail.com>
2025-11-04 14:30:54 +0330

    sprint 1 fixed

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\InformationController;
 Route::post('/register', [AuthController::class, 'registerPost'])->name('api.registerPost');
 Route::post('/login', [AuthController::class, 'loginPost'])->name('api.login');
 //Route::get('/', function () {return view('welcome');});
-//Route::get('/register' , [AuthController::class, 'register'])-> name('register');
+Route::get('/register' , [AuthController::class, 'registerPost'])-> name('register');
 //Route::get('/login' , [AuthController::class, 'login'])-> name('login');
 Route::middleware('auth:sanctum')->group(function () {
     //Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('api.dashboard');
